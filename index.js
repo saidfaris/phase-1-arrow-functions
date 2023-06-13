80a67bd18ca84db748212411faa107f512cb6a67
sidee <said.issack@student.moringaschool.com>
2023-06-14 00:29:12 +0300

    completed assighnment

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+    return 2000 / 100;
+  }
+  
+  const square = (x) => x * x;
+  
+  const add = (x, y) => x + y;
+  
+  console.log(divide()); // 20
+  console.log(square(5)); // 25
+  console.log(add(10, 20)); // 30
+  
